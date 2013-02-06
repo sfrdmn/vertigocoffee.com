1251b760bfd594221828d80de470b24d6bc0fa4f
Sean Fridman <fridman@mail.sfsu.edu>
2013-02-05 17:18:06 -0800

    Compass config bug >:|

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'compass-normalize'
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "ims"
+images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
