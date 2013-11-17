6523187042fcf8fb2fbaeb6ead535c2d1fa65a48
Sean Fridman <fridman@mail.sfsu.edu>
2013-11-16 16:08:09 -0800

    Initialized rails project

---
+++ vertigocoffee/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vertigocoffee::Application.config.secret_key_base = 'f86e151bdc3293f88bf57562c17041c816c057f17613f0e7b7437edaf467b4d35a64f209c848e1afa768d80dbe31f12bdbceff3f94a96dbf38541cc39082ec2d'
